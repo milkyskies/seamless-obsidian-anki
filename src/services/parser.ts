20364baf3544cea3cd58b9c5fb836a123c2bd86f
realonesc <ryanrmc@gmail.com>
2022-02-28 21:43:58 +0900

    Inserting text

--- src/services/parser.ts
+++ src/services/parser.ts
@@ -1,6 +1,7 @@
 import {
 	App,
 	Editor,
+	editorEditorField,
 	MarkdownView,
 	TFile,
 } from "obsidian";
@@ -21,26 +22,59 @@ export class Parser {
 	}
 
 	// https://regex101.com/r/OIZEIQ/3
+	// https://regex101.com/r/a1AtGP/2
 
 	public async generateCards(activeFile: TFile): Promise<BasicCard[]> {
 
 		const view = await this.app.workspace.getActiveViewOfType(MarkdownView);
 
 
-		const pattern = /(.+?)(?:>>)(.*?)(?:%%(?:.*?)(?:(anki(?:\((.*?)(?:\))).*)%%)|$)/gmi;
+		const pattern = /(.+?)(>>)(.*?)(%%(.*?(anki\((.*?)\)).*)?%%|$)/gmi;
 		//const text = await this.app.vault.read(activeFile);
-
-		const text = view.editor.getValue();
+		const editor = view.editor;
+		const text = editor.getValue();
 		let newText: string = text;
 		let match = pattern.exec(text);
 
-		newText = newText.replace(pattern, "yo")
+		//console.log(this.matchingLines(text, pattern));
+
+		// runs the code inside of the brackets for each line. they are all line numbers!!
+		this.matchingLines(text, pattern).forEach(function (line, index) {
+			match = pattern.exec(text);
+			if (match[4] == "") {
+				editor.setLine(line, match[1] + match[2] + match[3] + " %%anki(id:aaa)%%");
+			}
+			console.log("Oh yeah: " + match[4] + " (Line number " + line );
+		});
+
+
+		//newText = newText.replace(pattern, "yo")
+
+		//console.log(newText);
+		//
 		//while (match != null) {
-			//this.basiccards.push(new BasicCard(-1, match[1], match[2]));
-		//	console.log("Oh yeah: " + match[4]);
-		//	match = pattern.exec(text);
+			//this.basiccards.push(new BasicCard(-1, match[1], match[3]));
+			//console.log("Oh yeah: " + match[4]);
+			//newText = newText.replace(pattern, "yo")
+			//console.log(newText);
+			//console.log("hello");
+			//match = pattern.exec(text);
 		//}
-		view.editor.setValue(newText);
+		//view.editor.setValue(newText);
 		return this.basiccards;
+	}
+
+
+	private matchingLines(text: string, pattern: RegExp): number[] {
+		const matchingLines = [];
+		const allLines = text.split("\n");
+	
+		for (let i = 0; i < allLines.length; i++) {
+			if (allLines[i].match(pattern)) {
+				matchingLines.push(i);
+			}
+		}
+	
+		return matchingLines;
 	}
 }
